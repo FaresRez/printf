1dd6c6973fa7ebfaa3f7525cc9d6078adb93f923
sedkitech <dhiasedkipro@gmail.com>
2023-03-28 19:16:29 -0700

    optimizing main.h and adding va_end in printf

--- _printf.c
+++ _printf.c
@@ -1,8 +1,4 @@
-#include <unistd.h>
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 
 /**
  * len - length of a string
@@ -71,6 +67,7 @@ int _printf(const char *format, ...)
 		}
 	}
 
+	va_end(args);
 	return (length);
 
 }
--- main.h
+++ main.h
@@ -1,4 +1,11 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 int _printf(const char *format, ...);
+
 #endif
