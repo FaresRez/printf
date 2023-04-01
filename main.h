692214bc3fe9c3aa4d163c5bb2ad95c5a7390841
Fares <69602373+FaresRez@users.noreply.github.com>
2023-04-01 02:04:21 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,15 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
-
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-
 int _printf(const char *format, ...);
-<<<<<<< HEAD
+void print_int(int num);
 int len(char *ch);
-=======
-
->>>>>>> 1dd6c6973fa7ebfaa3f7525cc9d6078adb93f923
 #endif
